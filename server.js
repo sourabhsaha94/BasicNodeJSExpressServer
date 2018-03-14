03e423eaada909505fd4b6d6ce9736209b45fc0d
Sourabh Saha <sssaha2@ncsu.edu>
2018-03-14 23:21:42 +0000

    added fuzzer code

--- server.js
+++ server.js
@@ -1,12 +1,30 @@
 const express = require('express');
+var shell = require('shelljs');
 const app = express();
 const PORT = 8082;
+const COMMIT_LIMIT = 3;
+var commitCount=0;
 app.get('/',function(req,res){
   res.send("Api endpoints => /runFuzzer , /runTestAnalysis");
 });
 
 app.get('/runFuzzer',function(req,res){
-  res.send("Running the tool");
+  res.send("Running tool\n");
+  //to stop the build from continuing
+  if(commitCount<COMMIT_LIMIT)
+  {
+    shell.cd('/home/vagrant/iTrust2-v1/test/');
+    shell.rm('-rf','*');
+    shell.touch(String(commitCount));
+    shell.exec("git add .");
+    shell.exec(`git commit -m "test commit B${String(commitCount)}"`);
+    setTimeout(function(){  //delay required as it takes time for build to start and we dont want to revert before that
+      shell.cd('/home/vagrant/iTrust2-v1/')
+      shell.exec('git reset --hard HEAD~ ');
+    },30000);
+    commitCount++;//keeps track of how many builds are executed
+    shell.cd('/home/vagrant');
+  }
 });
 
 app.get('/runTestAnalysis',function(req,res){
